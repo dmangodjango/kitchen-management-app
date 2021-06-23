c370dbeac369f43f006ede35d6a41ad5519b1d41
Hafiz Mustafa <hafizmustafa@Hafizs-MacBook-Pro.local>
2021-06-24 02:00:13 +0800

    add elements to recipes list components

--- src/app/tabs/recipes-list/recipes-list-routing.module.ts
+++ src/app/tabs/recipes-list/recipes-list-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesListPage } from './recipes-list.page';
+import { ItemsListPage } from '../items-list/items-list.page';
+
 
 const routes: Routes = [
   {
     path: '',
     component: RecipesListPage
-  }
+  },
+
 ];
 
 @NgModule({
--- src/app/tabs/recipes-list/recipes-list.page.ts
+++ src/app/tabs/recipes-list/recipes-list.page.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListPage implements OnInit {
 
+  slideOptions = {
+    slidesPerView: 3,
+    spaceBetween: 5
+  };
+
+  itemCategory = [
+    {
+      category: "Breakfast",
+      selected: true
+    },
+    {
+      category: "Lunch",
+      selected: false
+
+    },    
+    {
+      category: "Snack",
+      selected: false
+    },
+    {
+      category: "Dinner",
+      selected: false
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeOnSelectedCategory(index) {
+    const previousSelected = this.itemCategory.findIndex(item => item.selected);
+    this.itemCategory[previousSelected].selected = false;
+    this.itemCategory[index].selected = true;
   }
 
 }
